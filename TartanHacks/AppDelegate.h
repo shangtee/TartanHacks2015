5a75007db44bdfb71fe4c3362f9e91b413f89085
Sophia Anopa <sanopa@gmail.com>
2015-02-06 18:07:18 -0500

    General project setup commit.

---
+++ TartanHacks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TartanHacks
+//
+//  Created by Sophia Anopa on 06.02.15.
+//  Copyright (c) 2015 SLZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
