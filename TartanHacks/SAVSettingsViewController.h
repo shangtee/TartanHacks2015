81f80a0895758e220a600d69e33da518df4dc036
Sophia Anopa <sanopa@gmail.com>
2015-02-07 02:36:33 -0500

    Adding the SettingsView.

---
+++ TartanHacks/SAVSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAVSettingsViewController.h
+//  TartanHacks
+//
+//  Created by Sophia Anopa on 07.02.15.
+//  Copyright (c) 2015 SLZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAVSettingsViewController : UIViewController
+
+@end
