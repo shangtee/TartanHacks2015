bdfc22e70de08b2ff2bda859e84c7f425ebeb8a6
Lillian Choung <lchoung@andrew.cmu.edu>
2015-02-07 17:12:01 -0500

    Slider alignment

--- TartanHacks/SAVFeedViewController.m
+++ TartanHacks/SAVFeedViewController.m
@@ -33,7 +33,7 @@
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(refreshTableView) forControlEvents:UIControlEventValueChanged];
     
-    self.radiusSlider = [[UISlider alloc] initWithFrame: CGRectMake(10, 30, self.view.frame.size.width - 100, 40)];
+    self.radiusSlider = [[UISlider alloc] initWithFrame: CGRectMake(35, 30, self.view.frame.size.width - 100, 40)];
     self.radiusSlider.value = [(NSNumber *)([PFUser currentUser][@"searchRadius"]) floatValue];
     self.radiusSlider.minimumValue = 0.0;
     self.radiusSlider.maximumValue = 10.0;
@@ -42,7 +42,7 @@
     [self.tableView.tableHeaderView addSubview:self.radiusSlider];
     
     self.radiusLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, self.view.frame.size.width, 30)];
-    self.radiusLabel.text = [NSString stringWithFormat:@"Search Radius: %d km", (int)self.radiusSlider.value];
+    self.radiusLabel.text = [NSString stringWithFormat:@"Search radius: %d km", (int)self.radiusSlider.value];
     self.radiusLabel.font = [UIFont fontWithName:@"Helvetica Light" size:15];
     [self.tableView.tableHeaderView addSubview:self.radiusLabel];
 
