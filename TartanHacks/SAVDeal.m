2209995582949e10894711c4fa8aca4d922dda65
Sophia Anopa <sanopa@gmail.com>
2015-02-06 21:22:58 -0500

    Person & Deal classes.

---
+++ TartanHacks/SAVDeal.m
@@ -0,0 +1,13 @@
+//
+//  SAVDeal.m
+//  TartanHacks
+//
+//  Created by Sophia Anopa on 06.02.15.
+//  Copyright (c) 2015 SLZA. All rights reserved.
+//
+
+#import "SAVDeal.h"
+
+@implementation SAVDeal
+
+@end
---
+++ TartanHacks/SAVPerson.h
@@ -0,0 +1,13 @@
+//
+//  SAVPerson.h
+//  TartanHacks
+//
+//  Created by Sophia Anopa on 06.02.15.
+//  Copyright (c) 2015 SLZA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAVPerson : NSObject
+
+@end
